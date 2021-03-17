52d221eaf504cf0e4255d5be134f1b3969420af2
Twelve <ellery.xii@gmail.com>
2021-03-17 23:16:46 +0800

    Assignment8 Done

--- Assignment8/src/rope.cpp
+++ Assignment8/src/rope.cpp
@@ -11,7 +11,7 @@ namespace CGL {
 
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
-        // Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+      // Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
         Vector2D interval = (end-start)/(double)num_nodes;
     
         masses.push_back(new Mass(start, node_mass, false));
@@ -19,7 +19,6 @@ namespace CGL {
             masses.push_back(new Mass(start+i*interval, node_mass, false));
             springs.push_back(new Spring(masses[i-1],masses[i],k));
         }
-
         for (auto &i : pinned_nodes) {
             masses[i]->pinned = true;
         }
@@ -29,12 +28,11 @@ namespace CGL {
     {
         for (auto &s : springs)
         {
-            //TODO (Part 2): Use Hooke's law to calculate the force on a node
-            auto a=s->m1->position, b = s->m2->position;
-            auto fab = -s->k*((a-b)/(a-b).norm())*((a-b).norm()-s->rest_length);
-            auto fba = -s->k*((b-a)/(b-a).norm())*((b-a).norm()-s->rest_length);
-            s->m1->forces+=fab;
-            s->m2->forces+=fba;
+            // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            auto ab =s->m2->position - s->m1->position;
+            auto f = s->k*(ab/ab.norm())*(ab.norm()-s->rest_length);
+            s->m1->forces+=f;
+            s->m2->forces-=f;
         }
 
         for (auto &m : masses)
@@ -42,13 +40,20 @@ namespace CGL {
             if (!m->pinned)
             {
                 // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                m->forces += gravity * m->mass;
+                
                 // TODO (Part 2): Add global damping
-                m->forces += m->mass * gravity;
-                m->velocity = m->velocity + m->forces*delta_t;
-                m->position = m->position+m->velocity*delta_t;
+                float k_d_global = 0.01;
+                m->forces += - k_d_global * m->velocity;
+                
+
+                Vector2D a = m->forces;
+
+                //semi-implicit Euler
+                m->velocity += a * delta_t;
+                m->position += m->velocity * delta_t;
 
             }
-
             // Reset all forces on each mass
             m->forces = Vector2D(0, 0);
         }
@@ -59,26 +64,26 @@ namespace CGL {
         for (auto &s : springs)
         {
             // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
-            double len = (s->m1->position - s->m2->position).norm() - s->rest_length;
-            len = len/2.0;
-            auto dir = (s->m1->position - s->m2->position)/(s->m1->position - s->m2->position).norm();
-            s->m2->forces += dir*len;
-            s->m1->forces -=dir*len;
+            auto ab =s->m2->position - s->m1->position;
+            auto f = s->k*(ab/ab.norm())*(ab.norm()-s->rest_length);
+            s->m1->forces+=f;
+            s->m2->forces-=f;
         }
 
         for (auto &m : masses)
         {
             if (!m->pinned)
             {
-                Vector2D temp_position = m->position;
-                m->forces += m->mass * gravity;
-                m->position += 0.9995*m->forces*delta_t*delta_t;
-                m->position += m->position-temp_position;
-
+                m->forces += gravity * m->mass;
+                auto a = m->forces / m->mass;
                 // TODO (Part 3.1): Set the new position of the rope mass
-                
+                auto temp_location = m->position;
                 // TODO (Part 4): Add global Verlet damping
+                float dampfactor = 0.00005;
+                m->position = m->position +  (1.0 - dampfactor) * (m->position - m->last_position) + a * delta_t *delta_t;
+                m->last_position = temp_location;
             }
+            m->forces =  Vector2D(0,0);
         }
     }
 }
