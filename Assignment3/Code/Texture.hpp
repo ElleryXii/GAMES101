743bb4c4c02b9f1f0de5bdc7fdabc2dad4103544
ElleryTwelve <ellery.xii@gmail.com>
2021-07-08 15:35:01 +0800

    clean up

--- Assignment3/Code/Texture.hpp
+++ Assignment3/Code/Texture.hpp
@@ -30,6 +30,17 @@ public:
         return Eigen::Vector3f(color[0], color[1], color[2]);
     }
 
+
+    Eigen::Vector3f getColor2(int v, int u)
+    {
+        int u_i = std::min(width - 1, u);
+        u_i = std::max(0, u_i);
+        int v_i = std::min(height - 1, v);
+        v_i = std::max(0, v_i);
+        auto color = image_data.at<cv::Vec3b>(v_i, u_i);
+        return Eigen::Vector3f(color[0], color[1], color[2]);
+    }
+
     Eigen::Vector3f getColorBilinear(float u, float v)
     {
         float u_img = u * (float)width;
@@ -47,39 +58,8 @@ public:
         Eigen::Vector3f c1 = p00 + s * (p10 - p00);
         Eigen::Vector3f c2 = p01 + s * (p11 - p01);
         Eigen::Vector3f color = c1 + t * (c2 - c1);
-        //int a = getColor(u, v) == Eigen::Vector3f(color[0], color[1], color[2]) ? 1 : 0;
-        //std::cout << a << std::endl;
         return color;
     }
-
-    Eigen::Vector3f getColor2(int v, int u)
-    {
-        int u_i = std::min(width - 1, u);
-        u_i = std::max(0, u_i);
-        int v_i = std::min(height - 1, v);
-        v_i = std::max(0, v_i);
-        auto color = image_data.at<cv::Vec3b>(v_i, u_i);
-        return Eigen::Vector3f(color[0], color[1], color[2]);
-    }
-
-    //Eigen::Vector3f getColorBilinear(float u, float v)
-    //{
-    //    float u_img = u * (width - 1);
-    //    float v_img = (1 - v) * (height - 1);
-
-    //    Eigen::Vector2f u00{ std::floor(u_img), std::floor(v_img) };
-    //    Eigen::Vector2f u01{ std::ceil(u_img), std::floor(v_img) };
-    //    Eigen::Vector2f u10{ std::floor(u_img), std::ceil(v_img) };
-    //    Eigen::Vector2f u11{ std::ceil(u_img), std::ceil(v_img) };
-
-    //    float s = u_img - std::floor(u_img);
-    //    float t = v_img - std::floor(v_img);
-
-    //    Eigen::Vector3f u0 = getColor2(u00.x(), u00.y()) + s * (getColor2(u01.x(), u01.y()) - getColor2(u00.x(), u00.y()));
-    //    Eigen::Vector3f u1 = getColor2(u10.x(), u10.y()) + s * (getColor2(u11.x(), u11.y()) - getColor2(u10.x(), u10.y()));
-
-    //    return u0 + t * (u1 - u0);
-    //}
 
 };
 #endif //RASTERIZER_TEXTURE_H
